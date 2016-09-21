491709532db07331f6d54a9f702a25ba8c7f3119
Lorin Hochstein <lhochstein@netflix.com>
2016-09-21 15:24:21 -0700

    initial commit

---
+++ sql.rb
@@ -0,0 +1,16 @@
+cheatsheet do
+    title 'sql'
+    docset_file_name 'sql'
+    keyword 'sql'
+    category do
+        id 'Insertions'
+        entry do
+            name 'Specify columns'
+            notes <<-'END'
+            ```sql
+            INSERT INTO mytable (colour, flavour) VALUES ('green', 'chocolate');
+            ```
+            END
+        end
+    end
+end
