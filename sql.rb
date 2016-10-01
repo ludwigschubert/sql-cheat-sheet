4928a7caf23fc19248c661294a8b54a7d0a6ac50
Lorin Hochstein <lhochstein@netflix.com>
2016-09-30 20:35:32 -0700

    deletions

--- sql.rb
+++ sql.rb
@@ -13,4 +13,23 @@ cheatsheet do
             END
         end
     end
+    category do
+        id 'Deletions'
+        entry do
+            name 'Delete all entries'
+            notes <<-'END'
+            ```sql
+            DELETE from mytable
+            ```
+            END
+        end
+        entry do
+            name 'Delete entries'
+            notes <<-'END'
+            ```sql
+            DELETE from mytable WHERE column=value;
+            ```
+            END
+        end
+    end
 end
